87c0d46c6f5ae6101d88151e3f83f71e6e6a133d
Mitchell Cooper <cooper879@msn.com>
2013-10-28 13:15:42 -0400

    xcode project created

---
+++ WeatherApp/WeatherApp/WAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  WAAppDelegate.h
+//  WeatherApp
+//
+//  Created by Mitchell Cooper on 10/28/13.
+//  Copyright (c) 2013 Really Good. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
