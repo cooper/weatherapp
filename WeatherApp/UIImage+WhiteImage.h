ccee067173412c16080acc41ce6ff7a4824c49de
Mitchell Cooper <githubspam@rlygd.net>
2018-09-07 13:26:42 -0700

    update

--- WeatherApp/UIImage+WhiteImage.h
+++ WeatherApp/UIImage+WhiteImage.h
@@ -1,5 +1,7 @@
 // Copyright (c) 2014 Mitchell Cooper.
- 
+
+#import <UIKit/UIKit.h>
+
 @interface UIImage (WhiteImage)
  
 - (UIImage *)whiteImage;
--- WeatherApp/UIImage+WhiteImage.m
+++ WeatherApp/UIImage+WhiteImage.m
@@ -25,7 +25,7 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
 //
- 
+
 #import "UIImage+WhiteImage.h"
 
 @implementation UIImage (WhiteImage)
